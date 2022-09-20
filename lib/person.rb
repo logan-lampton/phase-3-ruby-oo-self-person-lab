a0cf09909b6b8935099128986de8e761f07c033d
Logan Lampton <lampton.logan@gmail.com>
2022-09-20 16:56:45 -0400

    commit tree b844f6f5ef0bb25f21e8f78b8a95cec428e3a8ab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,29 @@
-# your code goes here
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(num)
+        @happiness = if num > 10
+                        10
+                    elsif num < 0
+                        0
+                    else
+                        num
+                    end
+
+    def hygiene=(num)
+        @hygiene = if num > 10
+                        10
+                    elsif num < 0
+                        0
+                    else
+                        num
+                    end
+end
